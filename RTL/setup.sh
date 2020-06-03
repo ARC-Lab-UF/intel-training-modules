c5818137a8dd15588f6845935ad48ccc72d047ce
Greg Stitt <gstitt@ece.ufl.edu>
2020-06-03 15:19:45 +0000

    Added error checking to setup script

--- RTL/setup.sh
+++ RTL/setup.sh
@@ -1,11 +1,13 @@
 REPO_PATH=~/intel-fpga-bbb
 INSTALL_PATH=~/
 
-if [ ! -d "$REPO_PATH" ]
+if [ -d "$REPO_PATH" ]
 then
-    git clone https://github.com/OPAE/intel-fpga-bbb $REPO_PATH
+    echo "Target folder $REPO_PATH already exists. Remove to ensure correct setup."
+    exit 1
 fi
 
+git clone https://github.com/OPAE/intel-fpga-bbb $REPO_PATH
 (cd $REPO_PATH; git checkout release/1.3.0; mkdir mybuild)
 (cd $REPO_PATH/mybuild; cmake ..)
 (cd $REPO_PATH/mybuild; make)
