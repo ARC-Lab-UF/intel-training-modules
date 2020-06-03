4c13d0d9b689543fc0b7fd80effab3b54e8ccc91
Greg Stitt <gstitt@ece.ufl.edu>
2020-06-03 15:45:48 +0000

    Added comments

--- RTL/setup.sh
+++ RTL/setup.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 REPO_PATH=~/intel-fpga-bbb
 INSTALL_PATH=~/
 
@@ -7,8 +9,13 @@ then
     exit 1
 fi
 
+# Clone the BBB repository into $REPO_PATH
 git clone https://github.com/OPAE/intel-fpga-bbb $REPO_PATH
+
+# Checkout a specific release of the repository and make the build directory
 (cd $REPO_PATH; git checkout release/1.3.0; mkdir mybuild)
+
+# Make and install the BBB modules to $INSTALL_PATH
 (cd $REPO_PATH/mybuild; cmake ..)
 (cd $REPO_PATH/mybuild; make)
 (cd $REPO_PATH/mybuild; make install DESTDIR=$INSTALL_PATH)
