d5265c0dccaf5d927e00204786752f83d329456b
Greg Stitt <gstitt@ece.ufl.edu>
2020-06-03 00:27:19 +0000

    Removed unused code from previous example.

--- RTL/exercises/simple_pipeline/code/sw/config.h
+++ RTL/exercises/simple_pipeline/code/sw/config.h
@@ -7,19 +7,6 @@
 //=============================================================
 // Configuration settings
 
-// Change "volatile int" to any data type you want to be stored in the input
-// and output arrays. Note that removal of the volatile qualifier requires
-// modification of main.cpp to use AFU::mallocNonvolatile(), which isn't 
-// guaranteed to work.
-typedef volatile int dma_data_t;
-
-// Other examples:
-//typedef volatile double dma_data_t;
-//typedef volatile unsigned char dma_data_t;
-// typedef int dma_data_t; // Must use AFU::mallocNonvolatile()
-// You can also use struct and class types, but will have to change the 
-// initialization and verification code in main.cpp.
-
 // When simulating, there is a loop that does nothing but wait for the DMA 
 // to finish. This constant "polling" is very inefficient and can slow down 
 // the CPU. Defining this flag causes the processor to periodically sleep
