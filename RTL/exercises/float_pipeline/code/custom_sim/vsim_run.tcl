c8a7d309726d4e7b0097d2368496dda1cb5d55d2
Greg Stitt <gstitt@ece.ufl.edu>
2020-06-03 00:42:11 +0000

    Fixed vsim_run.tcl

--- RTL/exercises/float_pipeline/code/custom_sim/vsim_run.tcl
+++ RTL/exercises/float_pipeline/code/custom_sim/vsim_run.tcl
@@ -1,6 +1,7 @@
 add wave -r /* 
-add wave -expand /ase_top/platform_shim_ccip_std_afu/ccip_std_afu/hal/afu/pipeline/inputs_r
-add wave -expand /ase_top/platform_shim_ccip_std_afu/ccip_std_afu/hal/afu/pipeline/mult_out
-add wave -expand /ase_top/platform_shim_ccip_std_afu/ccip_std_afu/hal/afu/pipeline/add_out_l1
-add wave -expand /ase_top/platform_shim_ccip_std_afu/ccip_std_afu/hal/afu/pipeline/add_out_l2
+# Add your own signals that you would like to appear in the waveform here:
+#add wave -expand /ase_top/platform_shim_ccip_std_afu/ccip_std_afu/hal/afu/pipeline/inputs_r
+#add wave -expand /ase_top/platform_shim_ccip_std_afu/ccip_std_afu/hal/afu/pipeline/mult_out
+#add wave -expand /ase_top/platform_shim_ccip_std_afu/ccip_std_afu/hal/afu/pipeline/add_out_l1
+#add wave -expand /ase_top/platform_shim_ccip_std_afu/ccip_std_afu/hal/afu/pipeline/add_out_l2
 run -all
