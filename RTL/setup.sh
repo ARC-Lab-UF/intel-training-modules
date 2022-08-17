fad95badd6c97dd90f090af1f66ac1c4b6ccc485
u93124 <u93124@s005-n007.aidevcloud>
2022-08-17 14:28:43 -0700

    Revised to use different BBB commit that works with Stratix 10

--- RTL/setup.sh
+++ RTL/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 REPO_PATH=~/intel-fpga-bbb
-INSTALL_PATH=~/
+INSTALL_PATH=~/intel-fpga-bbb-install
 
 if [ -d "$REPO_PATH" ]
 then
@@ -13,12 +13,14 @@ fi
 git clone https://github.com/OPAE/intel-fpga-bbb $REPO_PATH
 
 # Checkout a specific release of the repository and make the build directory
-(cd $REPO_PATH; git checkout release/1.3.0; mkdir mybuild)
+# (cd $REPO_PATH; git checkout release/1.3.0; mkdir mybuild)
+(cd $REPO_PATH; git checkout a422c8498ce7c3267ce287530317c5d4903d6845; mkdir mybuild)
+
 
 # Make and install the BBB modules to $INSTALL_PATH
-(cd $REPO_PATH/mybuild; cmake ..)
+(cd $REPO_PATH/mybuild; cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH ..)
 (cd $REPO_PATH/mybuild; make)
-(cd $REPO_PATH/mybuild; make install DESTDIR=$INSTALL_PATH)
+(cd $REPO_PATH/mybuild; make install)
 
 
 
