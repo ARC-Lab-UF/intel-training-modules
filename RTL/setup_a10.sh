16c9e23349af8f95f80ef478edab46f4a5da98cd
u93124 <u93124@s005-n007.aidevcloud>
2022-08-17 14:31:39 -0700

    Added new DevCloud setup scripts to use BBB and DMA

---
+++ RTL/setup_a10.sh
@@ -0,0 +1,10 @@
+export FPGA_BBB_CCI_SRC=~/intel-fpga-bbb
+export FPGA_BBB_CCI_INSTALL=~/intel-fpga-bbb-install
+export LD_LIBRARY_PATH=/glob/development-tools/versions/fpgasupportstack/a10/1.2.1/inteldevstack/a10_gx_pac_ias_1_2_1_pv/opencl/opencl_bsp/linux64/lib:/glob/development-tools/versions/fpgasupportstack/a10/1.2.1/intelFPGA_pro/hld/host/linux64/lib:/glob/development-tools/versions/fpgasupportstack/a10/1.2.1/intelFPGA_pro/hld/linux64/lib:$FPGA_BBB_CCI_INSTALL/lib64
+export AOCL_BOARD_PACKAGE_ROOT=/glob/development-tools/versions/fpgasupportstack/a10/1.2.1/inteldevstack/a10_gx_pac_ias_1_2_1_pv/opencl/opencl_bsp
+export ALTERAOCLSDKROOT=/glob/development-tools/versions/fpgasupportstack/a10/1.2.1/intelFPGA_pro/hld
+export QUARTUS_ROOTDIR_OVERRIDE=/glob/development-tools/versions/fpgasupportstack/a10/1.2.1/intelFPGA_pro/quartus
+export INTELFPGAOCLSDKROOT=/glob/development-tools/versions/fpgasupportstack/a10/1.2.1/intelFPGA_pro/hld
+export OPAE_PLATFORM_ROOT=/glob/development-tools/versions/fpgasupportstack/a10/1.2.1/inteldevstack/a10_gx_pac_ias_1_2_1_pv
+export PATH=/glob/intel-python/python2/bin:/glob/development-tools/versions/fpgasupportstack/a10/1.2.1/intelFPGA_pro/hld/bin:/glob/development-tools/versions/fpgasupportstack/a10/1.2.1/intelFPGA_pro/quartus/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin:/home/u93124/.local/bin:/home/u93124/bin:/bin:/glob/development-tools/versions/fpgasupportstack/a10/1.2.1/inteldevstack/a10_gx_pac_ias_1_2_1_pv/bin
+export QUARTUS_HOME=/glob/development-tools/versions/fpgasupportstack/a10/1.2.1/intelFPGA_pro/quartus
---
+++ RTL/setup_s10.sh
@@ -0,0 +1,11 @@
+export FPGA_BBB_CCI_SRC=~/intel-fpga-bbb
+export FPGA_BBB_CCI_INSTALL=~/intel-fpga-bbb-install
+export ALTERAOCLSDKROOT=/glob/development-tools/versions/fpgasupportstack/d5005/2.0.1/inteldevstack/hld
+export LD_LIBRARY_PATH=/glob/development-tools/versions/fpgasupportstack/d5005/2.0.1/inteldevstack/d5005_ias_2_0_1_b237/opencl/opencl_bsp/linux64/lib:/glob/development-tools/versions/fpgasupportstack/d5005/2.0.1/inteldevstack/hld/host/linux64/lib:/glob/development-tools/versions/fpgasupportstack/d5005/2.0.1/inteldevstack/hld/linux64/lib:$FPGA_BBB_CCI_INSTALL/lib64
+export INTELFPGAOCLSDKROOT=/glob/development-tools/versions/fpgasupportstack/d5005/2.0.1/inteldevstack/hld
+export PATH=/glob/intel-python/python2/bin:/glob/development-tools/versions/fpgasupportstack/d5005/2.0.1/inteldevstack/hld/bin:/glob/development-tools/versions/fpgasupportstack/d5005/2.0.1/inteldevstack/quartus/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin:/home/u93124/.local/bin:/home/u93124/bin:/bin:/glob/development-tools/versions/fpgasupportstack/d5005/2.0.1/inteldevstack/d5005_ias_2_0_1_b237/bin
+export QUARTUS_HOME=/glob/development-tools/versions/fpgasupportstack/d5005/2.0.1/inteldevstack/quartus
+export OPAE_PLATFORM_ROOT=/glob/development-tools/versions/fpgasupportstack/d5005/2.0.1/inteldevstack/d5005_ias_2_0_1_b237
+export QUARTUS_ROOTDIR_OVERRIDE=/glob/development-tools/versions/fpgasupportstack/d5005/2.0.1/inteldevstack/quartus
+export PAC_DMA_WORK_THREAD=yes
+export AOCL_BOARD_PACKAGE_ROOT=/glob/development-tools/versions/fpgasupportstack/d5005/2.0.1/inteldevstack/d5005_ias_2_0_1_b237/opencl/opencl_bsp
