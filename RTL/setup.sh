789ad55f615ed0b4e9311fb94aac9708d3fabdb9
Wesley Piard <1278350+wespiard@users.noreply.github.com>
2022-09-21 19:01:54 +0000

    Add cmake libdir prefix for consistency across different machines. Add prompt to delete existing BBB directory.

--- RTL/setup.sh
+++ RTL/setup.sh
@@ -5,23 +5,28 @@ INSTALL_PATH=~/intel-fpga-bbb-install
 
 if [ -d "$REPO_PATH" ]
 then
-    echo "Target folder $REPO_PATH already exists. Remove to ensure correct setup."
-    exit 1
+    echo "Target folder $REPO_PATH already exists."
+    read -n 1 -p "Would you like to remove it to continue setup? (y/n) " input
+    echo ""
+    case $input in  
+        y|Y) 
+            echo "Removing old folder."
+            rm -rf $REPO_PATH ;;
+        *)
+            echo "Exiting."
+            exit 1 ;; 
+    esac
 fi
 
 # Clone the BBB repository into $REPO_PATH
 git clone https://github.com/OPAE/intel-fpga-bbb $REPO_PATH
 
 # Checkout a specific release of the repository and make the build directory
-# (cd $REPO_PATH; git checkout release/1.3.0; mkdir mybuild)
-(cd $REPO_PATH; git checkout a422c8498ce7c3267ce287530317c5d4903d6845; mkdir mybuild)
+(cd $REPO_PATH; git checkout release/1.3.0; mkdir mybuild)
+# (cd $REPO_PATH; git checkout a422c8498ce7c3267ce287530317c5d4903d6845; mkdir mybuild)
 
 
 # Make and install the BBB modules to $INSTALL_PATH
-(cd $REPO_PATH/mybuild; cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH ..)
-(cd $REPO_PATH/mybuild; make)
+(cd $REPO_PATH/mybuild; cmake -DCMAKE_INSTALL_LIBDIR=lib64 -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH ..)
+(cd $REPO_PATH/mybuild; make -j)
 (cd $REPO_PATH/mybuild; make install)
-
-
-
-
